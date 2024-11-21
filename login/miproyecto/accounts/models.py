f7c51cc9997913511112acf65359b337d1d873d8
InaLiwi <paula.zelada@inacapmail.cl>
2024-11-21 11:16:56 -0300

    mas cambios en models

--- login/miproyecto/accounts/models.py
+++ login/miproyecto/accounts/models.py
@@ -4,11 +4,11 @@ from django.db import models
 #    libro_imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagennn', null=True, blank=True)
 class Usuario(models.Model):
     usuario_id = models.AutoField(max_length=11, primary_key=True)
-    usuario_rol = models.CharField(max_length=50, primary_key=True, verbose_name='Especifique el rol: ')
-    usuario_nombre_usuario = models.CharField(max_length=20, primary_key=True, verbose_name='Nombre usuario')
+    usuario_rol = models.CharField(max_length=50, verbose_name='Especifique el rol: ')
+    usuario_nombre_usuario = models.CharField(max_length=50, verbose_name='Nombre usuario')
     
     def __str__(self):
-        fila = "usuario: " + self.usuario_nombre + " || " + "Capacidad: " + str(self.usuario_capacidad) + " || " + "Ubicación: " + self.usuario_ubicacion
+        fila = "Usuario: " + self.usuario_nombre_usuario + " || " + "ID: " + str(self.usuario_id) + " || " + "Rol: " + self.usuario_rol
         return fila
     
     '''
@@ -23,10 +23,10 @@ class Ticket(models.Model):
     ticket_numero_cliente = models.ForeignKey(Usuario, on_delete=models.CASCADE, max_length=20, verbose_name='Nombre usuario')
     ticket_fecha = models.DateField(verbose_name='Fecha')
     ticket_prioridad = models.CharField(max_length=20, verbose_name='Prioridad (Alta-Media-Baja): ')
-    ticket_descripcion = models.FloatField(max_length=100, verbose_name='Descripción: ')
+    ticket_descripcion = models.CharField(max_length=100, verbose_name='Descripción: ')
 
-    def __str__(self): #En este caso, self.ticket_nombre_usuario es un objeto usuario (ya que ticket_nombre_usuario es una clave foránea). Por eso, hay que concatenarlo con str()
-        fila = "ticket: " + str(self.ticket_id) + " --- " + "usuario: " + str(self.ticket_nombre_usuario.usuario_nombre) + " || "  + "Usuario: " + self.ticket_nombre_usuario  + " || "  + str(self.ticket_fecha) + " a las " + str(self.ticket_hora) + " || "  + "Duración: " + str(self.ticket_duracion) + " hora(s)"
+    def __str__(self): #En este caso, self.ticket_numero_cliente es un objeto usuario (ya que ticket_numero_cliente es una clave foránea). Por eso, hay que concatenarlo con str()
+        fila = "ticket: " + str(self.ticket_id) + " --- " + "Usuario: " + str(self.ticket_numero_cliente.usuario_nombre_usuario) + " || "  + "Usuario: " + self.ticket_numero_cliente  + " || "  + str(self.ticket_fecha) + " || " + self.ticket_prioridad  + " || " + self.ticket_descripcion
         return fila
 
 
@@ -36,7 +36,7 @@ class Ticket(models.Model):
 #python manage.py migrate
 #aplica las migraciones creadas, sincroniza su estructura, modificaciones etc...
     
-# yoyo - 1234
+# yoyo - yoyo
     
     
     
