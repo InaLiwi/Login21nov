dcb2e25b083b8f67ffdac681af7e07a7c40eaa03
InaLiwi <paula.zelada@inacapmail.cl>
2024-11-21 10:55:23 -0300

    cambios en views

--- login/miproyecto/accounts/views.py
+++ login/miproyecto/accounts/views.py
@@ -1,6 +1,68 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render,  get_object_or_404
+from .models import *
+from .forms import *
 
 @login_required
 def home(request):
     return render(request, 'home.html')
+
+# USUARIOS
+def usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios/index.html', {'usuarios': usuarios})
+
+def crear_usuario(request):
+    formulario = UsuarioForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('usuarios')
+    return render(request, 'usuarios/crear.html', {'formulario':formulario})
+
+def editar_usuario(request, usuario_id):
+    usuario = get_object_or_404(Usuario, usuario_id=usuario_id)
+    if request.method == 'POST':
+        formulario = UsuarioForm(request.POST, request.FILES, instance=usuario)
+        if formulario.is_valid():
+            formulario.save()  
+            return redirect('usuarios')  
+    else:
+        formulario = usuarioForm(instance=usuario)  
+    return render(request, 'usuarios/editar.html', {'formulario': formulario})
+
+def eliminar_usuario(request, usuario_id):
+    usuario = Usuario.objects.get(usuario_id = usuario_id)
+    usuario.delete()
+    return redirect('usuarios')
+
+
+# TICKETS
+def tickets(request):
+    tickets = Ticket.objects.all()
+    return render(request, 'tickets/index.html', {'tickets': tickets})
+
+def crear_ticket(request):
+    formulario = TicketForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('tickets')
+    return render(request, 'tickets/crear.html', {'formulario':formulario})
+
+def editar_ticket(request, ticket_nombre):
+    ticket = get_object_or_404(Ticket, ticket_nombre=ticket_nombre)
+    if request.method == 'POST':
+        formulario = TicketForm(request.POST, request.FILES, instance=ticket)
+        if formulario.is_valid():
+            formulario.save()  
+            return redirect('tickets')  
+    else:
+        formulario = TicketForm(instance=ticket)  
+    return render(request, 'tickets/editar.html', {'formulario': formulario})
+
+def eliminar_ticket(request, ticket_nombre):
+    ticket = ticket.objects.get(ticket_nombre = ticket_nombre)
+    ticket.delete()
+    return redirect('tickets')
+    
